f4da823aa01a724d52b15af6dd7867e4050a1a3f
BettyyN <betelhemnegash10@gmail.com>
2024-06-03 14:13:04 +0300

    edit and delete functionality added

--- script.js
+++ script.js
@@ -1,25 +1,63 @@
-// handleEditTodo
-// handleDeleteTodo
-// handleAddTodo
-const todos = [
-  {
-    id: "1",
-    todoli: "washing dish",
-  },
-  {
-    id: "2",
-    todoli: "washing dish",
-  },
-  {
-    id: "3",
-    todoli: "washing dish",
-  },
-];
-
-let text="";
-todos.forEach(function (todo, index) {
-  text +=
-    "Todo " + (index + 1) + ": " + todo.todoli + "<br>";
+document.addEventListener("DOMContentLoaded", () => {
+  window.todos = [
+    { id: "1", todoli: "x" },
+    { id: "2", todoli: "y" },
+    { id: "3", todoli: "z" },
+  ];
+  updateTodoLists();
 });
 
-document.getElementById("demo").innerHTML = text;
+function handleAddTodo() {
+  const addtodo = document.getElementById("todobox");
+  const items = `    <input type="text" id="input1">
+       <Button onclick="insertTodo()">ADD</Button>
+        <Button onclick="cancelTodo()">CANCEL</Button>`;
+
+  addtodo.innerHTML = items;
+}
+
+
+function insertTodo() {
+  const input1 = document.getElementById("input1");
+  const value1 = input1.value.trim();
+  if (value1 !== "") {
+    window.todos.push({ id: (window.todos.length + 1).toString(), todoli: value1 });
+    input1.value = "";
+    updateTodoLists();
+    cancelTodo();
+  }
+}
+
+function updateTodoLists() {
+  const todoList = document.getElementById("todo-list");
+  todoList.innerHTML = ""; // Clear the current list
+  window.todos.forEach((todo) => {
+    const li = document.createElement("li");
+    li.innerHTML = `
+      <span id="todo">${todo.todoli}</span>
+      <button onclick="handleEditTodo('${todo.id}')">Edit</button>
+      <button onclick="handleDeleteTodo('${todo.id}')">Delete</button>
+    `;
+    todoList.appendChild(li);
+  });
+}
+function cancelTodo() {
+  const addtodo = document.getElementById("todobox");
+  addtodo.innerHTML = "";
+}
+function handleDeleteTodo(id) {
+ window.todos = window.todos.filter((todo) => todo.id !== id);
+ updateTodoLists();
+}
+
+
+function handleEditTodo(id) {
+  const todoToEdit = window.todos.find((todo) => todo.id === id);
+  const newTodo = prompt("Edit todo", todoToEdit.todoli);
+  if (newTodo) {
+    window.todos = window.todos.map((todo) =>
+      todo.id === id ? { ...todo, todoli: newTodo } : todo
+    );
+    updateTodoLists();
+  }
+}
